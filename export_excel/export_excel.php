225d75e2c9f76f5bc09d5cd129b144d637c37d1f
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-06-14 19:47:24 +0700

    make project multi_user

--- export_excel/export_excel.php
+++ export_excel/export_excel.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Export Data Ke Excel Dengan PHP </title>
+	<title>Export Data Ke Excel Dengan PHP - www.malasngoding.com</title>
 </head>
 <body>
 	<style type="text/css">
@@ -19,7 +19,7 @@
 
 	}
 	a{
-		background: blue;
+		background: green;
 		color: #fff;
 		padding: 8px 10px;
 		text-decoration: none;
@@ -33,76 +33,34 @@
 	?>
 
 	<center>
-		<h1>Export Data Ke Excel Dengan PHP <br/> www.malasngoding.com</h1>
+		<h1>Export Data Ke Excel Dengan PHP</h1>
 	</center>
 
 	<table border="1">
 		<tr>
 			<th>No</th>
-			<th>Nama Pegawai</th>
+			<th>Nama</th>
 			<th>Alamat</th>
 			<th>No.Telp</th>
 		</tr>
+		<?php 
+		// koneksi database
+		$koneksi = mysqli_connect("localhost","root","","pegawai");
+
+		// menampilkan data pegawai
+		$data = mysqli_query($koneksi,"select * from data_pegawai");
+		$no = 1;
+		while($d = mysqli_fetch_array($data)){
+		?>
 		<tr>
-			<td>1</td>
-			<td>Sulaiman</td>
-			<td>Jakarta</td>
-			<td>0829121223</td>
+			<td><?php echo $no++; ?></td>
+			<td><?php echo $d['nama']; ?></td>
+			<td><?php echo $d['alamat']; ?></td>
+			<td><?php echo $d['telepon']; ?></td>
 		</tr>
-		<tr>
-			<td>2</td>
-			<td>Diki Alfarabi Hadi</td>
-			<td>Jakarta</td>
-			<td>08291212211</td>
-		</tr>
-		<tr>
-			<td>3</td>
-			<td>Zakaria</td>
-			<td>Medan</td>
-			<td>0829121223</td>
-		</tr>
-		<tr>
-			<td>4</td>
-			<td>Alvinur</td>
-			<td>Jakarta</td>
-			<td>02133324344</td>
-		</tr>
-		<tr>
-			<td>5</td>
-			<td>Muhammad Rizani</td>
-			<td>Jakarta</td>
-			<td>08231111223</td>
-		</tr>
-		<tr>
-			<td>6</td>
-			<td>Rizaldi Waloni</td>
-			<td>Jakarta</td>
-			<td>027373733</td>
-		</tr>
-		<tr>
-			<td>7</td>
-			<td>Ferdian</td>
-			<td>Jakarta</td>
-			<td>0829121223</td>
-		</tr>
-		<tr>
-			<td>8</td>
-			<td>Fatimah</td>
-			<td>Jakarta</td>
-			<td>23432423423</td>
-		</tr>
-		<tr>
-			<td>9</td>
-			<td>Aminah</td>
-			<td>Jakarta</td>
-			<td>0829234233</td>
-		</tr>
-		<tr>
-			<td>10</td>
-			<td>Jafarudin</td>
-			<td>Jakarta</td>
-			<td>0829239323</td>
-		</tr>
+		<?php 
+		}
+		?>
 	</table>
 </body>
 </html>
