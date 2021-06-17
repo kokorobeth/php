ded60459c5443dec36ef35e30eec9c30f8b50d95
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-06-17 23:18:20 +0700

    tambah data faker

---
+++ tutorial/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '882b131ea9e90612b10b6531a57a2f4086ed7a6b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '882b131ea9e90612b10b6531a57a2f4086ed7a6b',
+    ),
+    'fzaninotto/faker' => 
+    array (
+      'pretty_version' => 'v1.9.2',
+      'version' => '1.9.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '848d8125239d7dbf8ab25cb7f054f1a630e68c2e',
+    ),
+  ),
+);
