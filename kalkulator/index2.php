7f3ab1d0c3cbbbdb118b39875d7ab3ddefda80be
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-06-20 17:05:24 +0700

    update file kalkulator

--- kalkulator/index2.php
+++ kalkulator/index2.php
@@ -22,15 +22,22 @@
 ?>
 
 <form action="index2.php" method="post">
+    <label for="bill">Input #1 Number</label>
     <input type="text" name="bil1" autocomplete="off">
+    <br>
+    <label for="bill">Input #2 Number</label>
     <input type="text" name="bil2" autocomplete="off">
+    <br>
+    <label for="operasi">Operation</label>
     <select name="operasi" id="">
         <option value="tambah">+</option>
         <option value="kurang">-</option>
         <option value="kali">*</option>
         <option value="bagi">/</option>
     </select>
-    <button type="submit" name="hitung">Hitung</button>
+    <button type="submit" name="hitung">Count</button>
+    <br>
+    <label for="hitung">Result</label>
     <?php if(isset($_POST['hitung'])): ?>
     <input type="text" name="hitung" value="<?php echo $hasil ?>">
     <?php else : ?>
